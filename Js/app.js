f2c7557b3fd1a922d894c2129ea345f2d4991973
Yeiby <armasyeison13@gmail.com>
2022-12-07 14:24:51 -0400

    Modificaciones varias

--- Js/app.js
+++ Js/app.js
@@ -1,53 +1,85 @@
-const p1 = document.querySelector("#p1");
-let urls = []
+const productos = document.querySelectorAll('.producto');
 let flag = false;
 let interval;
 
-const p1Img = p1.querySelector('img');
-urls = p1.dataset.srcs.split(',');
-p1Img.src = urls[0];
 
-p1Img.addEventListener('mouseover', itNext);
-p1Img.addEventListener('mouseout', itout);
+document.addEventListener('DOMContentLoaded', initApp);
 
-function itout(){
-    flag = false;
-    p1Img.src = urls[0];
-    clearInterval(inverval);
+function initApp(){
+    imgstock();
+   this.addEventListener('mouseover', itNext)
+   this.addEventListener('mouseout', itout)
 }
 
 
-function itNext(){
+
+function itout(e){
+
+    if(e.target.tagName === 'IMG'){
+        const p1 = e.target.parentElement;
+        const p1Img = e.target;
+       const urls = p1.dataset.srcs.split(',');
+       flag = false;
+       p1Img.src = urls[0];
+       clearInterval(inverval);
+    }
+   
+}
+
+
+function itNext(e){
+
+    if(e.target.tagName === 'IMG'){
+        flag = true;
+        timer = 0;
+       const p1 = e.target.parentElement;
+       const urls = p1.dataset.srcs.split(',');
+       const p1Img = e.target;
        let it = timeriterator(urls);
-       flag = true;
-       timer = 0;
+
+       
        if(flag){
        
           
 
 
-          inverval =  setInterval(function(){
-                timer++;
-                if(timer >= 5){
-                    timer = 0;
-                    p1Img.src = it.next().value != undefined ? it.next().value : urls[urls.length-1];
-                }
-                console.log(timer);
-            }, 300)
+        inverval =  setInterval(function(){
+              timer++;
+              if(timer >= 5){
+                  timer = 0;
+                  let url = it.next().value;
+                  if(url){
+                    p1Img.src = url;
+                  }
+              }
+              
+          }, 150)
 
 
 
-          
+        
+ 
+     }else{
+      p1Img.src = urls[0];
+      return false;
+     }
+    }
+
    
-       }else{
-        p1Img.src = urls[0];
-        return false;
-       }
+   
+      
+     
         
       
     
 }
 
+function imgstock(){
+    productos.forEach(element => {
+        urls = element.dataset.srcs.split(',');
+        element.querySelector('img').src = urls[0];
+    });
+}
 
 function* timeriterator(urls){
     for( url of urls){
